bcc04a938c25a3c1e77fba1d9ae9547273285ed9
ZYGA1 <145481853+ZYGA1@users.noreply.github.com>
2024-02-11 21:27:34 +0100

    console log dupa

--- dupa.js
+++ dupa.js
@@ -1,5 +1,6 @@
 var zdjencia = ['1.png','2.png','3.png','4.png'];
 var zdjenciaIndex=0
+console.log("dupa")
 function funkcja() {
     let tak = document.getElementById('szeli');
     zdjenciaIndex=(zdjenciaIndex+1) %zdjencia.length;
