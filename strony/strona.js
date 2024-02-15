05d6f9977916dddf03e272a96a50d8a6af3c063e
ZYGA <j.zyznowski123@gmail.com>
2024-02-15 20:30:21 +0100

    tak

---
+++ strony/strona.js
@@ -0,0 +1,19 @@
+zdjea = ['zadymiarze/1.png',
+'zadymiarze/2.png',
+'zadymiarze/3.png',
+'zadymiarze/4.png',
+'zadymiarze/5.png',
+'zadymiarze/6.png',
+'zadymiarze/7.png',
+'zadymiarze/8.png',
+'zadymiarze/9.png',
+'zadymiarze/10.png',
+'zadymiarze/11.png',
+'zadymiarze/12.png'];
+
+const dif  = document.getElementById("container")
+const zdje = document.createElement("img")
+zdje.src = zdjea[0];
+zdje.alt = 'Shelly'
+console.log(zdjea[0])
+zdje.appendChild(dif);
